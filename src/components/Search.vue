058ba8ac31f7782bedaebaa827748cc66e9c3795
Lemur <danilo_jevtovic@hotmail.com>
2022-03-04 21:34:10 +0100

    komponenta za pretragu

---
+++ src/components/Search.vue
@@ -0,0 +1,41 @@
+<template>
+  <input class="search-input" type="text" v-model="searchInput" />
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+import useCrud from "../composables/useCRUD.js";
+const props = defineProps({
+  url: String,
+});
+const emit = defineEmits(["searched"]);
+
+const searchInput = ref("");
+watch(searchInput, () => {
+  searchFun();
+});
+
+// nadgledanje ako se promjeni url (selektuje neka druga grupa)
+// - ponovi pretragu za tu novu grupu
+watch(
+  () => props.url,
+  () => {
+    // searchInput.value = "";
+    searchFun();
+  }
+);
+
+const { readFun } = useCrud();
+const searchFun = async () => {
+  if (searchInput.value !== "") {
+    console.log(searchInput.value);
+    let res = await readFun(props.url + searchInput.value);
+    emit("searched", res.data);
+  } else {
+    emit("searched", null);
+  }
+};
+</script>
+
+<style scoped>
+</style>
