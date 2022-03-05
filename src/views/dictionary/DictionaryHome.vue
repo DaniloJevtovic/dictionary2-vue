c49c40081c706cc2f2be67088c168d7dc594d62d
Lemur <danilo_jevtovic@hotmail.com>
2022-03-05 22:42:39 +0100

    home stranica za rjecnik

---
+++ src/views/dictionary/DictionaryHome.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <!-- lista rjecnika - sidebar -->
+    <div class="left">
+      <DictionariesList />
+    </div>
+
+    <div class="right">
+      <router-view :key="$route.path" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import DictionariesList from "./DictionariesList.vue";
+</script>
+
+<style scoped>
+.home {
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  border: 1px solid indigo;
+  padding: 4px;
+  min-height: 500px;
+}
+
+.left {
+  border: 1px solid orange;
+}
+
+.right {
+  border: 1px solid indigo;
+}
+</style>
