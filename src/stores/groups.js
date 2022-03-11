ac53a5d9294174a5074447d44b0bf82d1478b9d9
Lemur <danilo_jevtovic@hotmail.com>
2022-03-11 22:49:25 +0100

    update broja rjeci/recenica u grupi

--- src/stores/groups.js
+++ src/stores/groups.js
@@ -1,4 +1,7 @@
 import { defineStore } from "pinia";
+import useCRUD from "../composables/useCRUD";
+
+const { patchFun } = useCRUD();
 
 // grupe rjeci i recenica za rjecnik
 
@@ -30,6 +33,36 @@ export const useGroupStore = defineStore("groups", {
       } else {
         this.sgroups.splice(idx, 1);
       }
+    },
+
+    // id - id grupe kojoj se azurira broj stavki
+    //mode - da li eje uvecenje ili umanjenje
+    //type - da li e grupa rjeci ili grupa recenica
+    async updateNumOfItems(id, mode, type) {
+      let res;
+
+      if (type == "WGROUP") {
+        res = this.getWGroupById(id);
+        res.numOfItems =
+          mode == "increase" ? res.numOfItems + 1 : res.numOfItems - 1;
+      } else {
+        res = this.getSGroupById(id);
+        res.numOfItems =
+          mode === "increase" ? res.numOfItems + 1 : res.numOfItems - 1;
+      }
+
+      //poziv na bekendu
+      await patchFun("groups/" + id + "/num/" + res.numOfItems);
+    },
+  },
+
+  getters: {
+    getWGroupById: (state) => {
+      return (groupId) => state.wgroups.find((group) => group.id === groupId);
+    },
+
+    getSGroupById: (state) => {
+      return (groupId) => state.sgroups.find((group) => group.id === groupId);
     },
   },
 });
