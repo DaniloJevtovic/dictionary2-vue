9eb72de78c07d8fbd0081ce316774d210a7b2e7f
Lemur <danilo_jevtovic@hotmail.com>
2022-03-12 22:55:04 +0100

    izmjene u words store, pozivi ka bekendu

--- src/stores/words.js
+++ src/stores/words.js
@@ -1,21 +1,52 @@
 import { defineStore } from "pinia";
 
+import useCrud from "../composables/useCRUD.js";
+const { createFun, readFun, deleteFun } = useCrud();
+
+import { useGroupStore } from "./groups.js";
+const groupStore = useGroupStore();
+
 export const useWordStore = defineStore("words", {
   state: () => {
     return { words: [] };
   },
 
   actions: {
-    addWord(word) {
-      this.words.push(word);
+    async addWord(word) {
+      let res = await createFun("words", word);
+      this.words.push(res.data);
+      this.isChangeWg(word.wgId);
     },
 
-    updateWord(word, idx) {
+    async updateWord(word, idx) {
       this.words[idx] = word;
+      await createFun("words", word);
+      this.isChangeWg(word.wgId);
     },
 
-    removeWord(idx) {
+    async removeWord(idx, id) {
       this.words.splice(idx, 1);
+      await deleteFun("words", id);
+    },
+
+    isChangeWg(wgId) {
+      if (groupStore.activeWgId !== wgId) {
+        groupStore.activeWgId = wgId;
+        this.getWordsForWgroup(wgId);
+
+        //ovdje mozda pozvati update broja stavki u grupi
+        return true;
+      }
+    },
+
+    async getWordsForDictionary(dicId) {
+      let res = await readFun("words/dic/" + dicId);
+      this.words = res.data.content;
+    },
+
+    async getWordsForWgroup(wgId) {
+      let res = await readFun("words/wg/" + wgId);
+      this.words = res.data.content;
     },
   },
 });
