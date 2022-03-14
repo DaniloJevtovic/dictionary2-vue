ad00ae9373d4a693f4cb7ef690286d54cab024a6
Lemur <danilo_jevtovic@hotmail.com>
2022-03-14 21:48:17 +0100

    komponeta koja sadrzi grupu rjeci i recenica (tab)

---
+++ src/views/groups/AllGroups.vue
@@ -0,0 +1,75 @@
+<template>
+  <!-- tab za grupe rjeci i recenica -->
+  <div class="de">
+    <button
+      v-for="(_, tab) in tabs"
+      :key="tab"
+      :class="['tab-button', { active: currentTab === tab }]"
+      @click="currentTab = tab"
+    >
+      {{ tab }}
+    </button>
+
+    <KeepAlive>
+      <component
+        :is="tabs[currentTab]"
+        :dicId="dicId"
+        :gType="currentTab"
+        class="tab"
+      >
+      </component>
+    </KeepAlive>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import WGroup from "./WGroup.vue";
+import SGroup from "./SGroup.vue";
+
+const props = defineProps({
+  dicId: String,
+});
+
+const currentTab = ref("WGroup");
+
+const tabs = {
+  WGroup,
+  SGroup,
+};
+</script>
+
+<style scoped>
+.de {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  user-select: none;
+  overflow-x: auto;
+  text-align: left;
+}
+
+.tab-button {
+  padding: 3px 15px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  background: #f0f0f0cc;
+  margin-bottom: -1px;
+  margin-right: -1px;
+  width: 50%;
+}
+
+.tab-button.active {
+  background: rgb(0, 217, 255);
+  color: white;
+}
+
+.tab {
+  border: 1px solid #ccc;
+  padding: 10px;
+  min-height: 100px;
+}
+</style>
