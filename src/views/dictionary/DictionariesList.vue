85a999b88044a5ce315b452300502dd1238cafde
Lemur <danilo_jevtovic@hotmail.com>
2022-03-16 00:50:57 +0100

    izmjene u rjecniku

--- src/views/dictionary/DictionariesList.vue
+++ src/views/dictionary/DictionariesList.vue
@@ -3,8 +3,9 @@
     <!-- side panel -->
     <p>Lista rjecnika</p>
 
-    <div v-for="dictionary in dictionaries" :key="dictionary.id">
+    <div v-for="dictionary in dictionaryStore.dictionaries" :key="dictionary.id">
       <router-link
+        @click="dictionaryStore.dictionary = dictionary"
         :to="{
           name: 'Dictionary',
           params: { id: dictionary.id, name: dictionary.name },
@@ -13,23 +14,46 @@
         {{ dictionary.name }}
       </router-link>
     </div>
+
+    <button @click="showModal = true" class="new-btn">+</button>
+
+    <AddEditDictionaryModal
+      v-if="showModal"
+      :show="showModal"
+      :dictionary="newDictionary"
+      :mode="'new'"
+      @close="showModal = false"
+    />
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, reactive } from "vue";
 import useCrud from "../../composables/useCRUD.js";
+import { useDictionaryStore } from "../../stores/dictionaries.js";
+import AddEditDictionaryModal from "./AddEditDictionaryModal.vue";
 
 const dictionaries = ref([]);
 const { readFun } = useCrud();
+const dictionaryStore = useDictionaryStore();
 
 const getDictionaries = async () => {
   let res = await readFun("/dictionaries");
-  dictionaries.value = res.data;
+  // dictionaries.value = res.data;
+  dictionaryStore.dictionaries = res.data;
 };
 
 onMounted(() => {
   getDictionaries();
+});
+
+const showModal = ref(false);
+
+const newDictionary = reactive({
+  name: "",
+  desciption: "",
+  userId: "1",
+  color: "#ffffff",
 });
 </script>
 
--- src/views/dictionary/Dictionary.vue
+++ src/views/dictionary/Dictionary.vue
@@ -1,36 +1,61 @@
 <template>
-  <div>
-    <h3>{{ dictionary.name }}</h3>
+  <div class="main">
+    <div
+      @click="showModal = true"
+      :style="{ background: dictionaryStore.dictionary.color }"
+    >
+      <h3>
+        {{ dictionaryStore.dictionary.name }}
+      </h3>
+    </div>
 
     <div class="demo">
       <button
         v-for="(_, tab) in tabs"
         :key="tab"
-        :class="['tab-button', { active: currentTab === tab }]"
-        @click="currentTab = tab"
+        :class="['tab-button', { active: tabStore.currentTab === tab }]"
+        @click="tabStore.currentTab = tab"
       >
         {{ tab }}
       </button>
 
       <!-- DA TI SVAKI PUT NE MONTIRA KOMPONENTU -->
       <KeepAlive>
-        <component :is="tabs[currentTab]" :dicId="dictionary.id" class="tab">
+        <component
+          :is="tabs[tabStore.currentTab]"
+          :dicId="dictionary.id"
+          class="tab"
+        >
         </component>
       </KeepAlive>
     </div>
+
+    <AddEditDictionaryModal
+      v-if="showModal"
+      :show="showModal"
+      :dictionary="dictionaryStore.dictionary"
+      :mode="'update'"
+      @close="showModal = false"
+    />
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
+import { useGroupStore } from "../../stores/groups.js";
+import { useTabStore } from "../../stores/tabs.js";
+import { useDictionaryStore } from "../../stores/dictionaries.js";
+import AddEditDictionaryModal from "./AddEditDictionaryModal.vue";
 
 import Words from "../../views/words/Words.vue";
 import Sentences from "../../views/sentences/Sentences.vue";
 import AllGroups from "../groups/AllGroups.vue";
 import Grammars from "../../views/grammars/Grammars.vue";
 
-const currentTab = ref("Words");
+// const currentTab = ref("Words");
+
+const dictionaryStore = useDictionaryStore();
 
 const tabs = {
   Words,
@@ -39,12 +64,19 @@ const tabs = {
   Grammars,
 };
 
+const tabStore = useTabStore();
+const groupStore = useGroupStore();
+
+onMounted(() => {
+  groupStore.resetActiveGroups();
+  //tabStore.currentTab = "Words";
+});
+
 const route = useRoute();
 
 const dictionary = ref({ name: route.params.name, id: route.params.id });
 
-import { useTabStore } from "../../stores/tabs.js";
-const tabStore = useTabStore();
+const showModal = ref(false);
 </script>
 
 <style scoped>
