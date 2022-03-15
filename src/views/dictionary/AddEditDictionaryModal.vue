af2b01b2ec87a120d9b7779ee2c8770cee32b443
Lemur <danilo_jevtovic@hotmail.com>
2022-03-15 23:13:49 +0100

    modal za kreiranje i update rjecnika

---
+++ src/views/dictionary/AddEditDictionaryModal.vue
@@ -0,0 +1,80 @@
+<template>
+  <div v-if="show" class="modal-mask">
+    <div class="modal-wrapper">
+      <div class="modal-container">
+        <div class="modal-header">
+          <h3>{{ mode }} dictionary</h3>
+        </div>
+
+        <div class="modal-body">
+          <input
+            type="text"
+            v-model="updateDictionary.name"
+            placeholder="dictionary name"
+            required
+          />
+
+          <textarea
+            v-model="updateDictionary.description"
+            placeholder="description"
+            rows="5"
+          ></textarea>
+
+          <input type="color" v-model="updateDictionary.color" />
+
+          <br />
+        </div>
+
+        <div class="modal-footer">
+          <slot name="footer">
+            <button @click="save">save</button>
+            <button @click.prevent="closeModal">cancel</button>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import useCrud from "../../composables/useCRUD.js";
+import { useDictionaryStore } from "../../stores/dictionaries.js";
+
+const props = defineProps({
+  dictionary: Object,
+  show: Boolean,
+  mode: String,
+  idx: Number,
+});
+
+const emit = defineEmits(["close"]);
+
+const { createFun } = useCrud();
+const dictionaryStore = useDictionaryStore();
+
+const updateDictionary = reactive({ ...props.dictionary });
+
+async function save() {
+  let res = await createFun("dictionaries", updateDictionary);
+
+  if (props.mode === "new") {
+    dictionaryStore.addDictionary(res.data);
+    //preusmjeri na taj novi rjecnik
+  } else {
+    dictionaryStore.updateDictionary(res.data);
+  }
+
+  closeModal();
+}
+
+function closeModal() {
+  emit("close");
+}
+</script>
+
+<style scoped>
+.modal-container {
+  width: 500px;
+}
+</style>
