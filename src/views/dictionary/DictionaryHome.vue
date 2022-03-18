e1d36389a0e9dd74fb82a58da8031d6a929da4da
Lemur <danilo_jevtovic@hotmail.com>
2022-03-18 23:31:47 +0100

    izmjene na pocetnoj strani rjecnika

--- src/views/dictionary/DictionaryHome.vue
+++ src/views/dictionary/DictionaryHome.vue
@@ -1,34 +1,64 @@
 <template>
   <div class="home">
-    <!-- lista rjecnika - sidebar -->
-    <div class="left">
-      <DictionariesList />
+    <div class="navbar">
+      <button @click="$router.push({ name: 'User' })">Hi, Lemur</button>
+      <!-- <button @click="showList = !showList">Rjecnici</button> -->
+      <button @click="$router.push({ name: 'AllDictionaries' })">
+        Rjecnici
+      </button>
+      <button @click="$router.push('/login')">Logout</button>
     </div>
 
-    <div class="right">
+    <!-- <div v-if="showList" class="menu">
+      <DictionariesList @closeSidebar="showList = false" />
+    </div> -->
+
+    <div class="container">
       <router-view :key="$route.path" />
     </div>
   </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from "vue";
+import { useDictionaryStore } from "../../stores/dictionaries.js";
 import DictionariesList from "./DictionariesList.vue";
+
+import useCrud from "../../composables/useCRUD.js";
+import { useRouter } from "vue-router";
+
+const { readFun } = useCrud();
+const dictionaryStore = useDictionaryStore();
+
+const getDictionaries = async () => {
+  let res = await readFun("/dictionaries");
+  dictionaryStore.dictionaries = res.data;
+};
+
+onMounted(() => {
+  getDictionaries();
+});
+
+const showList = ref(false);
 </script>
 
 <style scoped>
 .home {
-  display: grid;
-  grid-template-columns: 1fr 5fr;
-  border: 1px solid indigo;
+  border: 1px solid rgb(36, 51, 73);
   padding: 4px;
-  min-height: 600px;
 }
 
-.left {
-  border: 1px solid orange;
+.navbar {
+  background: rgb(70, 51, 153);
 }
 
-.right {
-  border: 1px solid indigo;
+.menu {
+  margin: auto;
+  text-align: center;
+}
+
+.container {
+  border: 1px solid royalblue;
+  /* min-height: 500px; */
 }
 </style>
