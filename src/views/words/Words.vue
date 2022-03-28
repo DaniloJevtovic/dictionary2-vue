4355ccd581f6ffacb67b1a9945eea211f62c4cef
Lemur <danilo_jevtovic@hotmail.com>
2022-03-28 22:24:46 +0200

    izmjene u rjecima po ko zna koji put

--- src/views/words/Words.vue
+++ src/views/words/Words.vue
@@ -36,7 +36,7 @@
           </select>
 
           <!-- filter rjeci -->
-          <select
+          <!-- <select
             v-model="filterSelect"
             @change="changeFilter($event)"
             class="filter"
@@ -46,9 +46,22 @@
             <option value="favorite">favorite</option>
             <option value="a-z">[a-z]</option>
             <option value="z-a">[z-a]</option>
+          </select> -->
+
+          <select
+            v-model="wordStore.filter"
+            @change="changeFilter($event)"
+            class="filter"
+          >
+            <option value="sort=id,desc">newest</option>
+            <option value="sort=id,asc">oldest</option>
+            <option value="sort=favorite,desc">favorite</option>
+            <option value="sort=word,asc">[a-z]</option>
+            <option value="sort=word,desc">[z-a]</option>
           </select>
         </div>
 
+        <!-- nova rjec -->
         <button
           @click="(showModal = true), (newWord.wgId = groupStore.activeWgId)"
           class="new-btn"
@@ -65,22 +78,27 @@
           +{{ searchInput }}
         </button>
 
+        <!-- rjeci -->
         <div v-for="(word, index) in wordStore.words" :key="word.id">
           <Word :word="word" :idx="index" />
         </div>
 
-        <!-- ucitavanje jos rjeci -->
+        <!-- dugme ucitavanje jos rjeci -->
         <button
-          v-if="currentPage + 1 < totalPages"
+          v-if="
+            wordStore.currentPage + 1 < wordStore.totalPages &&
+            searchInput === ''
+          "
           @click="loadMoreWords"
           class="new-btn"
           style="width: 100%"
         >
-          load more words [{{ currentPage + 1 }} / {{ totalPages }}]
+          load more words [{{ wordStore.currentPage + 1 }} /
+          {{ wordStore.totalPages }}]
         </button>
       </div>
 
-      <!-- prikaz grupa -->
+      <!-- prikaz grupa - desna strana -->
       <div class="wgroups">
         <WGroup :dicId="dicId" :gType="'WGROUP'" />
       </div>
@@ -114,13 +132,12 @@ const { readFun } = useCrud();
 const wordStore = useWordStore();
 const groupStore = useGroupStore();
 
-const totalPages = ref("");
-const currentPage = ref(0);
 // dobavljanje rjeci za rjecnik ili grupu
 const words = ref([]);
 async function getWords(url) {
   let res = await readFun(url);
-  totalPages.value = res.data.totalPages;
+  wordStore.currentPage = 0;
+  wordStore.totalPages = res.data.totalPages;
   words.value = res.data.content;
   wordStore.words = res.data.content;
 }
@@ -133,19 +150,23 @@ async function getWGroups() {
 
 onMounted(() => {
   getWGroups();
-  getWords("words/dic/" + props.dicId);
+  wordStore.filter = "sort=id,desc";
+  //getWords("words/dic/" + props.dicId);
+  wordStore.getWords("DIC", props.dicId);
 });
 
 function changeWg(event) {
   let id = event.target.value;
 
   if (id === "all") {
-    getWords("words/dic/" + props.dicId);
+    //getWords("words/dic/" + props.dicId);
+    wordStore.getWords("DIC", props.dicId);
   } else {
-    getWords("words/wg/" + id);
+    // getWords("words/wg/" + id);
+    wordStore.getWords("WG", id);
   }
 
-  currentPage.value = 0;
+  wordStore.currentPage = 0;
   filterSelect.value = "newest";
   groupStore.activeWgId = newWord.wgId = id;
 }
@@ -192,64 +213,80 @@ const filterSelect = ref("newest");
 
 function changeFilter(event) {
   let filter = event.target.value;
-  let sortFilter;
 
-  switch (filter) {
-    case "newest":
-      sortFilter = "/?sort=id,desc";
-      break;
-    case "oldest":
-      sortFilter = "/?sort=id,asc";
-      break;
-    case "favorite":
-      sortFilter = "/?sort=favorite,desc";
-      break;
-    case "a-z":
-      sortFilter = "/?sort=word,asc";
-      break;
-    case "z-a":
-      sortFilter = "/?sort=word,desc";
-      break;
-  }
+  wordStore.filter = filter;
+
+  // let sortFilter;
+
+  // switch (filter) {
+  //   case "newest":
+  //     sortFilter = "/?sort=id,desc";
+  //     wordStore.filter = "sort=id,desc";
+  //     break;
+  //   case "oldest":
+  //     sortFilter = "/?sort=id,asc";
+  //     wordStore.filter = "sort=id,asc";
+  //     break;
+  //   case "favorite":
+  //     sortFilter = "/?sort=favorite,desc";
+  //     wordStore.filter = "sort=favorite,desc";
+  //     break;
+  //   case "a-z":
+  //     sortFilter = "/?sort=word,asc";
+  //     wordStore.filter = "sort=word,asc";
+  //     break;
+  //   case "z-a":
+  //     sortFilter = "/?sort=word,desc";
+  //     wordStore.filter = "sort=word,desc";
+  //     break;
+  // }
 
   if (groupStore.activeWgId === "all") {
-    let dicUrl = "words/dic/" + props.dicId + sortFilter;
-    getWords(dicUrl);
+    // let dicUrl = "words/dic/" + props.dicId + sortFilter;
+    // getWords(dicUrl);
+    wordStore.getWords("DIC", props.dicId);
   } else {
-    let wgUrl = "words/wg/" + groupStore.activeWgId + sortFilter;
-    getWords(wgUrl);
+    // let wgUrl = "words/wg/" + groupStore.activeWgId + sortFilter;
+    // getWords(wgUrl);
+    wordStore.getWords("WG", groupStore.activeWgId);
   }
 
-  currentPage.value = 0;
+  wordStore.currentPage = 0;
 }
 
 //ucitavanje jos rjeci sa bekenda (paginacija)
 async function loadMoreWords() {
-  currentPage.value++;
+  // wordStore.currentPage++;
 
   if (groupStore.activeWgId === "all") {
-    let res = await readFun(
-      "words/dic/" + props.dicId + "/?page=" + currentPage.value
-    );
+    // let res = await readFun(
+    //   "words/dic/" + props.dicId + "/?page=" + wordStore.currentPage
+    // );
 
-    //konkatenacija ne radi iz nekog razloga pa mora ovako :/ - popraviti!
-    //words.value.concat(res.data.content);
-    res.data.content.forEach((word) => {
-      words.value.push(word);
-    });
+    // console.log(res.data.content);
 
-    wordStore.words = words.value;
+    // //konkatenacija ne radi iz nekog razloga pa mora ovako :/ - popraviti!
+    // //words.value.concat(res.data.content);
+    // res.data.content.forEach((word) => {
+    //   words.value.push(word);
+    // });
+
+    // wordStore.words = words.value;
+    wordStore.loadMoreWords("DIC", props.dicId);
   } else {
-    let res = await readFun(
-      "words/wg/" + groupStore.activeWgId + "/?page=" + currentPage.value
-    );
+    // let res = await readFun(
+    //   "words/wg/" + groupStore.activeWgId + "/?page=" + wordStore.currentPage
+    // );
 
-    //konkatenacija ne radi iz nekog razloga pa mora ovako :/ - popraviti
-    res.data.content.forEach((word) => {
-      words.value.push(word);
-    });
+    // //konkatenacija ne radi iz nekog razloga pa mora ovako :/ - popraviti
+    // //wordStore.words.concat(res.data.content);
+    // res.data.content.forEach((word) => {
+    //   words.value.push(word);
+    // });
 
-    wordStore.words.concat(res.data.content);
+    // wordStore.words = words.value;
+
+    wordStore.loadMoreWords("WG", groupStore.activeWgId);
   }
 }
 </script>
