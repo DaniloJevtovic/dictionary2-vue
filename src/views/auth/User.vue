bb4f09b450c2d115997562af77cfeaa154c6bf6c
Lemur <danilo_jevtovic@hotmail.com>
2022-04-08 00:39:41 +0200

    komponenta za korisnikove podatke

---
+++ src/views/auth/User.vue
@@ -0,0 +1,22 @@
+<template>
+  <form>
+    <h4 style="margin: 0px; background: cyan; color: darkslateblue">
+      User settings
+    </h4>
+
+    <input type="text" placeholder="full name" required />
+    <input type="text" placeholder="your language" />
+    <input type="email" placeholder="email" required />
+    <input type="password" placeholder="password" required />
+    <input type="password" placeholder="repeat password" required />
+
+    <button @click="saveChanges">Save changes</button>
+  </form>
+</template>
+
+<script setup>
+async function saveChanges() {}
+</script>
+
+<style scoped>
+</style>
