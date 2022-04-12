e55a17625c3fb9e6eccf1da812d92993ab3eacff
Lemur <danilo_jevtovic@hotmail.com>
2022-04-12 12:12:12 +0200

    komponente za tipove rjeci

---
+++ src/views/wordtypes/WordType.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="type">{{ type.name.toLowerCase() }}</div>
+</template>
+
+<script setup>
+const props = defineProps({
+  type: Object,
+  idx: Number,
+});
+</script>
+
+<style scoped>
+.type {
+  margin: 3px;
+  /* border: 1px solid darkblue; */
+  background: v-bind("props.type.color");
+  padding: 2px;
+  display: block;
+  /* border: 2px solid v-bind("props.type.color"); */
+  border-radius: 2px;
+
+}
+</style>
---
+++ src/views/wordtypes/WordTypes.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="type-div">
+    <h4 style="margin: 2px">word types</h4>
+    <div class="types">
+      <div v-for="(type, index) in typeStore.types" :key="type.name">
+        <!-- {{ type.name }} -->
+
+        <WordType :type="type" :idx="index" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useWordTypeStore } from "../../stores/wordtypes.js";
+import WordType from "./WordType.vue";
+
+const typeStore = useWordTypeStore();
+
+// const wordTypes = [
+//   { name: "NOUN", color: "springgreen" },
+//   { name: "PRONOUN", color: "cyan" },
+//   { name: "PRONOUN", color: "purple" },
+//   { name: "ADJECTIVE", color: "orange" },
+//   { name: "VERB", color: "blue" },
+//   { name: "ADVERB", color: "yellow" },
+//   { name: "PREPOSITION", color: "hotpink" },
+//   { name: "CONJUNCTION", color: "orangered" },
+//   { name: "INTERJECTION", color: "skyblue" },
+// ];
+</script>
+
+<style scoped>
+.type-div {
+  border: 1px solid darkblue;
+  padding: 4px;
+  margin-top: 4px;
+}
+
+.types {
+  border: 1px solid orange;
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
