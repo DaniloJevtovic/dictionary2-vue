4421581225f94d9dd33a2c7523c4b1aaa705a25a
Lemur <danilo_jevtovic@hotmail.com>
2022-04-28 23:18:47 +0200

    izmjene u komponenti za korisnika

--- src/views/auth/User.vue
+++ src/views/auth/User.vue
@@ -1,20 +1,27 @@
 <template>
-  <form>
-    <h4 style="margin: 0px; background: cyan; color: darkslateblue">
-      User settings
-    </h4>
+  <div>
+    <form>
+      <h4 style="margin: 0px; background: cyan; color: darkslateblue">
+        User settings
+      </h4>
 
-    <input type="text" placeholder="full name" required />
-    <input type="text" placeholder="your language" />
-    <input type="email" placeholder="email" required />
-    <input type="password" placeholder="password" required />
-    <input type="password" placeholder="repeat password" required />
+      <input type="text" placeholder="full name" required />
+      <input type="text" placeholder="your language" />
+      <input type="email" placeholder="email" required />
+      <input type="password" placeholder="password" required />
+      <input type="password" placeholder="repeat password" required />
+      <input type="text" placeholder="your language" />
 
-    <button @click="saveChanges">Save changes</button>
-  </form>
+      <button @click="saveChanges">Save changes</button>
+    </form>
+
+    <AppSettings />
+  </div>
 </template>
 
 <script setup>
+import AppSettings from "./AppSettings.vue";
+
 async function saveChanges() {}
 </script>
 
