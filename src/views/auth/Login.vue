24fd77823fda8b02e241a438239b276d0066d5fc
Lemur <danilo_jevtovic@hotmail.com>
2022-05-03 01:01:25 +0200

    dodata polja u formama za prijavu i registraciju

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Login</div>
+  <form>
+    <h4 style="margin: 0px; background: cyan; color: darkslateblue">Login</h4>
+
+    <input type="email" placeholder="email" required />
+    <input type="password" placeholder="password" required />
+
+    <button>Login</button>
+  </form>
 </template>
 
 <script setup>
--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>Register</div>
+  <form>
+    <h4 style="margin: 0px; background: cyan; color: darkslateblue">
+      Register
+    </h4>
+
+    <input type="text" placeholder="full name" required />
+    <input type="text" placeholder="your language" />
+    <input type="email" placeholder="email" required />
+    <input type="password" placeholder="password" required />
+    <input type="password" placeholder="repeat password" required />
+    <input type="text" placeholder="your language" />
+
+    <button>Register</button>
+  </form>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
+.register-form {
+  width: 50%;
+  margin: auto;
+}
 </style>
