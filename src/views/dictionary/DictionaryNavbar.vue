c480221a50be13ce6da562d7098b995064cc045b
Lemur <danilo_jevtovic@hotmail.com>
2022-05-10 22:55:04 +0200

    navbar rjecnika kao posebna kompoenenta

---
+++ src/views/dictionary/DictionaryNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar">
+    <router-link :to="{ name: 'User' }"> Hi, Lemur </router-link> |
+    <!-- <button @click="showList = !showList">Rjecnici</button> -->
+    <router-link :to="{ name: 'AllDictionaries' }"> Rjecnici </router-link> |
+    <router-link :to="{ name: 'Login' }">Logout</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
