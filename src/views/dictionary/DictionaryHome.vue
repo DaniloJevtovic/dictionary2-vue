bdc2a9fac7789afcaaf8dc8e82997d848955cf4b
Lemur <danilo_jevtovic@hotmail.com>
2022-05-18 01:15:29 +0200

    obrisano suvisno na pocetnoj strani rjecnika

--- src/views/dictionary/DictionaryHome.vue
+++ src/views/dictionary/DictionaryHome.vue
@@ -9,37 +9,15 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
-import { useDictionaryStore } from "../../stores/dictionaries.js";
 import DictionaryNavbar from "./DictionaryNavbar.vue";
-
-import useCrud from "../../composables/useCRUD.js";
-import { useRouter } from "vue-router";
-
-const { readFun } = useCrud();
-const dictionaryStore = useDictionaryStore();
-
-const getDictionaries = async () => {
-  let res = await readFun("/dictionaries");
-  dictionaryStore.dictionaries = res.data;
-};
-
-onMounted(() => {
-  getDictionaries();
-});
-
-const showList = ref(false);
 </script>
 
 <style scoped>
 .home {
-  /* border: 1px solid rgb(36, 51, 73); */
-  padding: 4px;
+  padding: 5px;
 }
 
 .container {
-  /* border: 1px solid red; */
   height: 100%;
-  /* min-height: 500px; */
 }
 </style>
