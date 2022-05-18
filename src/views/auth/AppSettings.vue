74e4f1b55003a3b9530aa77ef98a3f74c9fe4286
Lemur <danilo_jevtovic@hotmail.com>
2022-05-19 01:20:52 +0200

    podesavanja prikaza rjeci/recenica - grupa

--- src/views/auth/AppSettings.vue
+++ src/views/auth/AppSettings.vue
@@ -6,26 +6,60 @@
 
     <p>dictionary direction -> yourt languate -> foreign language</p>
 
-    <div>
-      <input
-        type="radio"
-        @select="settingsStore.wgroupDirection = 'left'"
-        v-model="settingsStore.wgroupDirection"
-        id="wg"
-        name="wg"
-        value="left"
-      />
-      <label for="html">words -> groups</label><br />
+    <div class="settings">
+      <h4 style="margin: 0px">Word settings</h4>
+      <div class="radio-settings">
+        <div>
+          <input
+            type="radio"
+            @select="settingsStore.wgroupDirection = 'left'"
+            v-model="settingsStore.wgroupDirection"
+            id="w-g"
+            name="w-g"
+            value="left"
+          />
+          <label for="w-g">words -> groups</label><br />
+        </div>
+        <div>
+          <input
+            type="radio"
+            @select="settingsStore.wgroupDirection = 'right'"
+            v-model="settingsStore.wgroupDirection"
+            id="g-w"
+            name="g-w"
+            value="right"
+          />
+          <label for="g-w">groups -> words</label><br />
+        </div>
+      </div>
+    </div>
 
-      <input
-        type="radio"
-        @select="settingsStore.wgroupDirection = 'right'"
-        v-model="settingsStore.wgroupDirection"
-        id="css"
-        name="wg"
-        value="right"
-      />
-      <label for="css">groups -> words</label><br />
+    <div class="settings">
+      <h4 style="margin: 0px">Sentence settings</h4>
+      <div class="radio-settings">
+        <div>
+          <input
+            type="radio"
+            @select="settingsStore.sgroupDirection = 'left'"
+            v-model="settingsStore.sgroupDirection"
+            id="s-g"
+            name="s-g"
+            value="left"
+          />
+          <label for="s-g">sentences -> groups</label><br />
+        </div>
+        <div>
+          <input
+            type="radio"
+            @select="settingsStore.sgroupDirection = 'right'"
+            v-model="settingsStore.sgroupDirection"
+            id="g-s"
+            name="g-s"
+            value="right"
+          />
+          <label for="g-s">groups -> sentences</label><br />
+        </div>
+      </div>
     </div>
 
     <br />
@@ -41,4 +75,13 @@ const settingsStore = useSettingsStore();
 </script>
 
 <style scoped>
+.settings {
+  border: 1px solid darkgray;
+  margin-top: 3px;
+}
+
+.radio-settings {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
 </style>
