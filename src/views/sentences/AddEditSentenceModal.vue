22b29ddd15dacc076158199068aa9df1c498c060
Lemur <danilo_jevtovic@hotmail.com>
2022-05-29 19:54:17 +0200

    kreiranja nove grupe unutar dijaloga za recenice

--- src/views/sentences/AddEditSentenceModal.vue
+++ src/views/sentences/AddEditSentenceModal.vue
@@ -2,7 +2,7 @@
   <div v-if="show" class="modal-mask">
     <div class="modal-wrapper">
       <div class="modal-container">
-        <div class="modal-header">
+        <div class="modal-header" :class="mode">
           <h3>{{ mode }} sentence</h3>
         </div>
 
@@ -26,27 +26,30 @@
             rows="5"
           ></textarea>
 
-          <select
-            class="sgs"
-            v-model="updateSentence.sgId"
-            :style="{
-              background:
-                updateSentence.sgId != 'all'
-                  ? groupStore.getSGroupById(updateSentence.sgId).color
-                  : 'white',
-            }"
-            required
-          >
-            <option value="all" disabled>odaberi</option>
-            <option
-              v-for="group in groupStore.sgroups"
-              :key="group.id"
-              :value="group.id"
-              :style="{ background: group.color }"
+          <div class="group-select">
+            <select
+              class="sgs"
+              v-model="updateSentence.sgId"
+              :style="{
+                background:
+                  updateSentence.sgId != 'all'
+                    ? groupStore.getSGroupById(updateSentence.sgId).color
+                    : 'white',
+              }"
+              required
             >
-              Grupa: {{ group.name }} -- [{{ group.numOfItems }}]
-            </option>
-          </select>
+              <option value="all" disabled>odaberi</option>
+              <option
+                v-for="group in groupStore.sgroups"
+                :key="group.id"
+                :value="group.id"
+                :style="{ background: group.color }"
+              >
+                Grupa: {{ group.name }} -- [{{ group.numOfItems }}]
+              </option>
+            </select>
+            <button @click="showModal = true" class="new-group-btn">+</button>
+          </div>
 
           <br />
         </div>
@@ -59,12 +62,21 @@
         </div>
       </div>
     </div>
+
+    <AddEditGroupModal
+      v-if="showModal"
+      :show="showModal"
+      :group="newGroup"
+      :mode="'new'"
+      @close="switchSg"
+    />
   </div>
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import useCrud from "../../composables/useCRUD.js";
+import AddEditGroupModal from "../../views/groups/AddEditGroupModal.vue";
 import { useSentenceStore } from "../../stores/sentences.js";
 import { useGroupStore } from "../../stores/groups.js";
 
@@ -104,6 +116,28 @@ async function save() {
   closeModal();
 }
 
+const numOfGroups = ref(groupStore.sgroups.length);
+function switchSg() {
+  //provjera da li je dodata nova grupe ako jeste - prebaci na tu grupu
+  if (groupStore.sgroups.length > numOfGroups.value) {
+    console.log("vece je");
+    updateSentence.sgId = groupStore.sgroups[0].id;
+  }
+
+  showModal.value = false;
+}
+
+const showModal = ref(false);
+
+const newGroup = reactive({
+  name: "",
+  description: "",
+  dicId: props.sentence.dicId,
+  color: "#ffffff",
+  numOfItems: 0,
+  type: "SGROUP",
+});
+
 function closeModal() {
   emit("close");
 }
@@ -112,5 +146,18 @@ function closeModal() {
 <style scoped>
 .modal-container {
   width: 300px;
+}
+
+.group-select {
+  display: flex;
+  align-content: center;
+  align-items: center;
+}
+
+.new-group-btn {
+  padding: 3px;
+  margin-left: 3px;
+  background: blue;
+  color: cyan;
 }
 </style>
