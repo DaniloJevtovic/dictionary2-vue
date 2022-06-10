667bcc816d11f930e541024a79ffe88d8e4327f7
Lemur <danilo_jevtovic@hotmail.com>
2022-06-10 21:52:05 +0200

    back dugme

--- src/views/auth/User.vue
+++ src/views/auth/User.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div :class="settingsStore.dark === true ? 'dark-theme' : 'white-theme'">
+    <button @click="$router.go(-1)" class="back-btn">&#8592; Go Back</button>
+
     <form>
       <h4 style="margin: 0px; background: cyan; color: darkslateblue">
         User settings
@@ -16,14 +18,23 @@
     </form>
 
     <AppSettings />
+
+    <br>
   </div>
 </template>
 
 <script setup>
+import { useSettingsStore } from "../../stores/settings.js";
 import AppSettings from "./AppSettings.vue";
+
+const settingsStore = useSettingsStore();
 
 async function saveChanges() {}
 </script>
 
 <style scoped>
+.back-btn {
+  margin-top: 20px;
+  background: cyan;
+}
 </style>
