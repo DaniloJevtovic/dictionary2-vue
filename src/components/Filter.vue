63354ecb984ee2d4559462278ed3e27070908d08
Lemur <danilo_jevtovic@hotmail.com>
2022-06-10 21:52:19 +0200

    male izmjene u komponenti filtera

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,13 +1,15 @@
 <template>
   <select v-model="filterModel" @change="changeFilter($event)" class="filter">
     <option value="sort=id,desc">&#10041; newest</option>
-    <option value="sort=id,asc">oldest</option>
+    <option value="sort=id,asc">&bull; oldest</option>
     <option value="sort=favorite,desc">&#x2665; favorite</option>
     <option :value="'sort=' + type + ',asc'">&#8595; [a-z]</option>
     <option :value="'sort=' + type + ',desc'">&#8593; [z-a]</option>
 
-    <option v-if="type === 'word'" value="sort=wgId, asc">wgroup</option>
-    <option v-else value="sort=sgId, asc">sgroup</option>
+    <option v-if="type === 'word'" value="sort=wgId, asc">
+      &#9634; wgroup
+    </option>
+    <option v-else value="sort=sgId, asc">&#9634; sgroup</option>
   </select>
 </template>
 
