c1e682c9415e3f72baca07ef073fdd4295ce0e96
Lemur <danilo_jevtovic@hotmail.com>
2022-06-15 03:35:12 +0200

    komponenta razbijena na vise manjih

--- src/views/sentences/Sentences.vue
+++ src/views/sentences/Sentences.vue
@@ -2,102 +2,22 @@
   <div>
     <div class="sentences-sgs">
       <div class="sentences">
-        <div class="search-sg">
-          <!-- pretraga -->
-          <input type="text" v-model="searchInput" placeholder="search" />
+        <!-- search, groups, filter -->
+        <SentenceHeader :dicId="dicId" />
 
-          <!-- selekcija grupe -->
-          <select
-            class="sgs"
-            @change="changeSg($event)"
-            v-model="groupStore.activeSgId"
-            :style="{
-              background:
-                groupStore.activeSgId != 'all'
-                  ? groupStore.getSGroupById(groupStore.activeSgId).color
-                  : dictionaryStore.dictionary.color,
-              textAlign: 'center',
-            }"
-          >
-            <option value="all">sve recenice</option>
-            <option
-              v-for="group in groupStore.sgroups"
-              :key="group.id"
-              :value="group.id"
-              :style="{ background: group.color }"
-            >
-              Grupa: {{ group.name }} -- [{{ group.numOfItems }}s]
-            </option>
-          </select>
-
-          <Filter
-            :type="'sentence'"
-            :filterModel="sentenceStore.filter"
-            @filter="changeFilter2"
-          />
-
-          <button
-            @click="
-              (showModal = true), (newSentence.sgId = groupStore.activeSgId)
-            "
-            class="new-sentence"
-            :style="{
-              background:
-                groupStore.activeSgId != 'all'
-                  ? groupStore.getSGroupById(groupStore.activeSgId).color
-                  : dictionaryStore.dictionary.color,
-              textAlign: 'center',
-            }"
-          >
-            new
-          </button>
-        </div>
-
+        <!-- recenice -->
         <div class="sentences-list">
-          <!-- <button
-            @click="
-              (showModal = true), (newSentence.sgId = groupStore.activeSgId)
-            "
-            class="new-btn"
-            :style="{
-              background:
-                groupStore.activeSgId != 'all'
-                  ? groupStore.getSGroupById(groupStore.activeSgId).color
-                  : dictionaryStore.dictionary.color,
-            }"
-          >
-            + new sentence
-          </button> -->
+          <SentenceDescription />
 
-          <div
-            class="new-sentence-div"
-            @click="
-              (showModal = true), (newSentence.sgId = groupStore.activeSgId)
-            "
-          >
-            <div class="details">
-              <small>
-                <p>sentence &#8594; translate</p>
-                <p>
-                  <small>sentence description</small> |
-                  <small>sentence group</small>
-                </p>
-                <!-- <p>
-                  <small
-                    ><small>clik inside this div to add new sentence</small></small
-                  >
-                </p> -->
-              </small>
-            </div>
-          </div>
-
-          <!-- recenice -->
+          <!-- lista recenica -->
           <div
             v-for="(sentence, index) in sentenceStore.sentences"
             :key="sentence.id"
           >
             <Sentence :sentence="sentence" :idx="index" />
           </div>
+
+          <LoadMoreSentences />
         </div>
       </div>
 
@@ -105,34 +25,26 @@
         <SGroup :dicId="dicId" :gType="'SGROUP'" />
       </div>
     </div>
-
-    <!-- modal -->
-    <AddEditSentenceModal
-      v-if="showModal"
-      :show="showModal"
-      :sentence="newSentence"
-      :mode="'new'"
-      @close="showModal = false"
-    />
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref, reactive, watch } from "vue";
-import useCrud from "../../composables/useCRUD.js";
+import { onMounted } from "vue";
+
+import SentenceHeader from "./SentencesHeader.vue";
+import SentenceDescription from "./SentenceDescription.vue";
 import Sentence from "./Sentence.vue";
+import LoadMoreSentences from "./LoadMoreSentences.vue";
 import SGroup from "../groups/SGroup.vue";
-import AddEditSentenceModal from "./AddEditSentenceModal.vue";
+
 import { useSentenceStore } from "../../stores/sentences.js";
 import { useGroupStore } from "../../stores/groups.js";
 import { useDictionaryStore } from "../../stores/dictionaries.js";
-import Filter from "../../components/Filter.vue";
 
 const props = defineProps({
   dicId: String,
 });
 
-const { readFun } = useCrud();
 const sentenceStore = useSentenceStore();
 const groupStore = useGroupStore();
 const dictionaryStore = useDictionaryStore();
@@ -144,81 +56,12 @@ onMounted(() => {
   sentenceStore.search = "";
   sentenceStore.getSentences("DIC", props.dicId);
 });
-
-function changeSg(event) {
-  let id = event.target.value;
-
-  groupStore.activeSgId = newSentence.sgId = id;
-  sentenceStore.currentPage = 0;
-
-  if (id == "all") {
-    sentenceStore.getSentences("DIC", props.dicId);
-  } else {
-    sentenceStore.getSentences("SG", id);
-  }
-}
-
-const showModal = ref(false);
-
-const newSentence = reactive({
-  sentence: "",
-  translate: "",
-  description: "",
-  dicId: props.dicId,
-  sgId: "all",
-});
-
-const showGroups = ref(false);
-
-//filter
-const filterSelect = ref("newest");
-
-function changeFilter2(filter) {
-  sentenceStore.filter = filter;
-
-  if (groupStore.activeSgId === "all") {
-    sentenceStore.getSentences("DIC", props.dicId);
-  } else {
-    sentenceStore.getSentences("SG", groupStore.activeSgId);
-  }
-
-  sentenceStore.currentPage = 0;
-}
-
-//pretraga
-const searchInput = ref("");
-
-watch(searchInput, () => {
-  if (searchInput.value) {
-    sentenceStore.search = searchInput.value;
-    sentenceStore.searchSentences();
-  } else {
-    sentenceStore.search = "";
-
-    groupStore.getSGroupsForDictionary(props.dicId);
-
-    if (groupStore.activeSgId !== "all") {
-      sentenceStore.getSentences("SG", groupStore.activeSgId);
-    } else {
-      sentenceStore.getSentences("DIC", props.dicId);
-    }
-  }
-});
 </script>
 
 <style scoped>
 .sentences {
   /* overflow-y: auto; */
   border: 1px solid darkgray;
-}
-
-.search-sg {
-  display: flex;
-  gap: 5px;
-  /* margin: 4px; */
-  padding: 8px;
-  align-items: center;
-  border-bottom: 2px solid slateblue;
 }
 
 .sentences-sgs {
@@ -233,32 +76,13 @@ watch(searchInput, () => {
   max-height: 480px;
 }
 
-.new-sentence-div {
-  border: 1px solid rgb(111, 111, 180);
-  /* border-radius: 3px; */
-  margin-top: 4px;
-  padding: 2px;
-  text-align: center;
-  display: flex;
-}
-
 @media only screen and (max-width: 700px) {
-  .search-sg {
-    display: block;
-  }
-
   .sentences-sgs {
     display: block;
   }
 
-  .new-sentence {
-    /* display: none; */
-    display: block;
-    width: 100%;
-  }
-
-  .new-sentence::after {
-    content: "sentence";
+  .sentences-list {
+    max-height: 100%;
   }
 
   /* .filter {
