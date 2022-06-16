46e23ca5176bec90a6f16b5bbec482f1cd4d89f9
Lemur <danilo_jevtovic@hotmail.com>
2022-06-16 11:36:47 +0200

    komponenta za ucitavanje vise recenica

---
+++ src/views/sentences/LoadMoreSentences.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="load">
+    <button
+      v-if="
+        sentenceStore.currentPage + 1 < sentenceStore.totalPages &&
+        sentenceStore.search === ''
+      "
+      @click="loadMoreSentences"
+      class="new-btn"
+    >
+      load more sentences [{{ sentenceStore.currentPage + 1 }} /
+      {{ sentenceStore.totalPages }}]
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { useDictionaryStore } from "../../stores/dictionaries.js";
+import { useGroupStore } from "../../stores/groups.js";
+import { useSentenceStore } from "../../stores/sentences.js";
+
+const dictionaryStore = useDictionaryStore();
+const groupStore = useGroupStore();
+const sentenceStore = useSentenceStore();
+
+async function loadMoreSentences() {
+  if (groupStore.activeWgId === "all") {
+    sentenceStore.loadMoreSentences("DIC", dictionaryStore.dictionary.id);
+  } else {
+    sentenceStore.loadMoreSentences("WG", groupStore.activeSgId);
+  }
+}
+</script>
+
+<style scoped>
+.load {
+  margin-top: 4px;
+}
+
+.new-btn {
+  background: v-bind("groupStore.getWgColor()");
+  text-align: "center";
+}
+</style>
