ac17f86a2fe9c2c795c6f72776f8d6be8fcf9870
Lemur <danilo_jevtovic@hotmail.com>
2022-06-16 08:33:29 +0200

    pretraga za rjeci, recenice i gramatiku

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,44 +3,68 @@
     class="search-input"
     type="text"
     v-model="searchInput"
-    placeholder="pretraga"
+    :placeholder="'search ' + type"
   />
 </template>
 
 <script setup>
 import { ref, watch } from "vue";
-import useCrud from "../composables/useCRUD.js";
+
+import { useDictionaryStore } from "../stores/dictionaries.js";
+import { useGroupStore } from "../stores/groups.js";
+import { useWordStore } from "../stores/words.js";
+import { useSentenceStore } from "../stores/sentences.js";
+import { useGrammarStore } from "../stores/grammars.js";
+
+const dictionaryStore = useDictionaryStore();
+const groupStore = useGroupStore();
+const wordStore = useWordStore();
+const sentenceStore = useSentenceStore();
+const grammarStore = useGrammarStore();
 
 const props = defineProps({
-  url: String,
+  type: String,
 });
-const emit = defineEmits(["searched"]);
 
 const searchInput = ref("");
+
 watch(searchInput, () => {
-  searchFun();
-});
-
-// nadgledanje ako se promjeni url (selektuje neka druga grupa)
-// - ponovi pretragu za tu novu grupu
-watch(
-  () => props.url,
-  () => {
-    // searchInput.value = "";
-    searchFun();
-  }
-);
-
-const { readFun } = useCrud();
-const searchFun = async () => {
-  if (searchInput.value !== "") {
-    console.log(searchInput.value);
-    let res = await readFun(props.url + searchInput.value);
-    emit("searched", res.data);
+  if (searchInput.value) {
+    if (props.type === "words") {
+      wordStore.search = searchInput.value;
+      wordStore.searchWords();
+    } else if (props.type === "sentences") {
+      sentenceStore.search = searchInput.value;
+      sentenceStore.searchSentences();
+    } else {
+      grammarStore.search = searchInput.value;
+      grammarStore.searchGrammars();
+    }
   } else {
-    emit("searched", null);
+    if (props.type === "words") {
+      wordStore.search = "";
+      groupStore.getWGroupsForDictionary(dictionaryStore.dictionary.id);
+
+      if (groupStore.activeWgId !== "all") {
+        wordStore.getWords("WG", groupStore.activeWgId);
+      } else {
+        wordStore.getWords("DIC", dictionaryStore.dictionary.id);
+      }
+    } else if (props.type === "sentences") {
+      sentenceStore.search = "";
+      groupStore.getSGroupsForDictionary(dictionaryStore.dictionary.id);
+
+      if (groupStore.activeSgId !== "all") {
+        sentenceStore.getSentences("SG", groupStore.activeSgId);
+      } else {
+        sentenceStore.getSentences("DIC", dictionaryStore.dictionary.id);
+      }
+    } else {
+      grammarStore.search = "";
+      grammarStore.getGrammars();
+    }
   }
-};
+});
 </script>
 
 <style scoped>
