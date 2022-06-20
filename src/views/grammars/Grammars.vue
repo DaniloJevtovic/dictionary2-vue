703d7c229d4d094494ca8455f17ce579c5942b17
Lemur <danilo_jevtovic@hotmail.com>
2022-06-20 09:04:07 +0200

    lista gramatika u vise manjih komponenti

--- src/views/grammars/Grammars.vue
+++ src/views/grammars/Grammars.vue
@@ -1,59 +1,33 @@
 <template>
   <div>
     <div class="grammars-div">
-      <div class="search-gr">
-        <input
-          type="text"
-          v-model="searchInput"
-          placeholder="search by title"
-        />
-        <button
-          @click="showModal = true"
-          class="new-grammar"
-          :style="{ background: dictionaryStore.dictionary.color }"
-        >
-          new
-        </button>
-      </div>
-
-      <div class="new-grammar-div" @click="showModal = true">
-        <div class="details">
-          <small>
-            <p>grammar title</p>
-            <p>
-              <small>grammar content</small>
-            </p>
-          </small>
-        </div>
-      </div>
+      <GrammarHeader />
 
       <div class="grammars-list">
+        <GrammarDescription />
+
         <div
           v-for="(grammar, index) in grammarStore.grammars"
           :key="grammar.id"
         >
           <Grammar :grammar="grammar" :idx="index" />
         </div>
+
+        <LoadMoreGrammars />
       </div>
     </div>
-
-    <AddEditGrammarModal
-      v-if="showModal"
-      :show="showModal"
-      :grammar="newGrammar"
-      :mode="'new'"
-      @close="showModal = false"
-    />
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref, reactive, watch } from "vue";
-
-import Grammar from "./Grammar.vue";
-import AddEditGrammarModal from "./AddEditGrammarModal.vue";
+import { onMounted, ref, reactive } from "vue";
 import { useGrammarStore } from "../../stores/grammars.js";
 import { useDictionaryStore } from "../../stores/dictionaries.js";
+
+import GrammarHeader from "./GrammarsHeader.vue";
+import GrammarDescription from "./GrammarDescription.vue";
+import Grammar from "./Grammar.vue";
+import LoadMoreGrammars from "./LoadMoreGrammars.vue";
 
 const props = defineProps({
   dicId: String,
@@ -67,26 +41,12 @@ onMounted(() => {
 });
 
 const newGrammar = reactive({
-  title: "",
+  grammar: "",
   content: "",
   dicId: props.dicId,
 });
 
 const showModal = ref(false);
-
-//pretraga
-const searchInput = ref("");
-
-watch(searchInput, () => {
-  if (searchInput.value) {
-    grammarStore.search = searchInput.value;
-    grammarStore.searchGrammars();
-  } else {
-    grammarStore.search = "";
-
-    grammarStore.getGrammars();
-  }
-});
 </script>
 
 <style scoped>
@@ -95,33 +55,11 @@ watch(searchInput, () => {
   border: 1px solid darkgray;
 }
 
-.search-gr {
-  display: flex;
-  padding: 10px;
-  align-items: center;
-  justify-content: center;
-  gap: 5px;
-  border-bottom: 1px solid slateblue;
-}
-
 .grammars-list {
-  margin: 4px;
-}
-
-.new-grammar-div {
-  border: 1px solid rgb(111, 111, 180);
-  /* border-radius: 3px; */
-  margin: 4px;
-  padding: 2px;
-  text-align: center;
-  display: flex;
+  margin: 10px;
 }
 
 @media only screen and (max-width: 700px) {
-  .search-gr {
-    display: block;
-  }
-
   .new-grammar {
     width: 100%;
   }
