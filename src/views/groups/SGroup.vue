1dc993cf80d5890ebfac0f7cd3f7a4d888e8fe06
Lemur <danilo_jevtovic@hotmail.com>
2022-06-20 08:57:24 +0200

    grupe rjeci i recenica na vise komponenti

--- src/views/groups/SGroup.vue
+++ src/views/groups/SGroup.vue
@@ -1,40 +1,19 @@
 <template>
   <div class="sgroups">
-    <!-- <input type="text" v-model="searchInput" placeholder="search" /> -->
-
-    <div class="sg-header">
-      <h4 style="margin: 2px">sentence groups</h4>
-
-      <button
-        @click.prevent="showModal = true"
-        class="new-group"
-        :style="{ background: dictionaryStore.dictionary.color }"
-      >
-        + new group
-      </button>
-    </div>
+    <GroupsHeader :gType="'SGROUP'" />
 
     <div class="groups">
       <div v-for="(group, index) in groupStore.sgroups" :key="group.id">
         <Group :group="group" :idx="index" />
       </div>
     </div>
-
-    <AddEditGroupModal
-      v-if="showModal"
-      :show="showModal"
-      :group="newGroup"
-      :mode="'new'"
-      @close="showModal = false"
-    />
   </div>
 </template>
 
 <script setup>
-import { onMounted, reactive, ref } from "vue";
-import useCrud from "../../composables/useCRUD.js";
+import { onMounted } from "vue";
+import GroupsHeader from "./GroupsHeader.vue";
 import Group from "./Group.vue";
-import AddEditGroupModal from "./AddEditGroupModal.vue";
 import { useGroupStore } from "../../stores/groups.js";
 import { useDictionaryStore } from "../../stores/dictionaries.js";
 
@@ -43,59 +22,33 @@ const props = defineProps({
   gType: String,
 });
 
-const { readFun } = useCrud();
 const groupStore = useGroupStore();
 const dictionaryStore = useDictionaryStore();
 
-async function getGroups() {
-  let res = await readFun("groups/dic/" + props.dicId + "/group/SGROUP");
-  groupStore.sgroups = res.data;
-}
-
 onMounted(() => {
-  getGroups();
+  groupStore.getSGroupsForDictionary(props.dicId);
 });
-
-const searchInput = ref("");
-
-const newGroup = reactive({
-  name: "",
-  description: "",
-  dicId: props.dicId,
-  color: "#ffffff",
-  numOfItems: 0,
-  type: "SGROUP",
-});
-
-const showModal = ref(false);
 </script>
 
 <style scoped>
 .sgroups {
   border: 1px solid darkgray;
-  padding: 10px;
+  /* padding: 10px; */
 }
 
 .groups {
+  padding: 4px;
   overflow-y: auto;
   max-height: 480px;
 }
 
-.sg-header {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 5px;
-  margin: 10px;
+::-webkit-scrollbar-thumb {
+  background: v-bind("groupStore.getSgColor()");
 }
 
 @media only screen and (max-width: 700px) {
-  .sg-header {
-    display: block;
-  }
-
-  .new-group {
-    width: 100%;
+  .groups {
+    max-height: 100%;
   }
 }
 </style>
--- src/views/groups/WGroup.vue
+++ src/views/groups/WGroup.vue
@@ -2,45 +2,21 @@
   <div class="wgroups">
     <!-- <input type="text" v-model="searchInput" placeholder="search wgroup" /> -->
 
-    <div class="wg-header">
-      <h4 style="margin: 2px">word groups</h4>
-
-      <button
-        @click.prevent="showModal = true"
-        class="new-group"
-        :style="{ background: dictionaryStore.dictionary.color }"
-      >
-        + new group
-      </button>
-    </div>
+    <GroupsHeader :gType="'WGROUP'" />
 
     <div class="groups">
       <div v-for="(group, index) in groupStore.wgroups" :key="group.id">
         <Group :group="group" :idx="index" />
       </div>
     </div>
-
-    <!-- <div class="groups">
-      <div v-for="(group, index) in searchedWgs" :key="group.id">
-        <Group :group="group" :idx="index" />
-      </div>
-    </div> -->
-
-    <AddEditGroupModal
-      v-if="showModal"
-      :show="showModal"
-      :group="newGroup"
-      :mode="'new'"
-      @close="showModal = false"
-    />
   </div>
 </template>
 
 <script setup>
-import { onMounted, reactive, ref, watch } from "vue";
+import { onMounted, ref, watch } from "vue";
 
 import Group from "./Group.vue";
-import AddEditGroupModal from "./AddEditGroupModal.vue";
+import GroupsHeader from "./GroupsHeader.vue";
 import { useGroupStore } from "../../stores/groups.js";
 import { useDictionaryStore } from "../../stores/dictionaries.js";
 
@@ -52,75 +28,31 @@ const props = defineProps({
 const groupStore = useGroupStore();
 const dictionaryStore = useDictionaryStore();
 
-const searchedWgs = ref([]);
-
 onMounted(() => {
   if (dictionaryStore.dictionary.id !== props.dicId) {
-    //getWgroups(); // dobavljanje za bekenda
-    searchedWgs.value = groupStore.getWGroupsForDictionary(props.dicId);
-  }
-
-  searchedWgs.value = groupStore.wgroups;
-});
-
-const searchInput = ref("");
-
-watch(searchInput, () => {
-  console.log(searchInput.value);
-
-  if (searchInput.value !== "") {
-    // searchedWgs.value = groupStore.wgroups.filter((wg) =>
-    //   wg.name.includes(searchInput.value)
-    // );
-
-    searchedWgs.value = JSON.parse(JSON.stringify(groupStore.wgroups)).filter(
-      (wg) => wg.name.includes(searchInput.value)
-    );
-  } else {
-    searchedWgs.value = [...groupStore.wgroups];
+    groupStore.getWGroupsForDictionary(props.dicId);
   }
 });
-
-const newGroup = reactive({
-  name: "",
-  description: "",
-  dicId: props.dicId,
-  color: "#ffffff",
-  numOfItems: 0,
-  type: "WGROUP",
-});
-
-const showModal = ref(false);
 </script>
 
 <style scoped>
 .wgroups {
   border: 1px solid darkgray;
-  padding: 8px;
-}
-
-.wg-header {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 5px;
-  margin: 10px;
+  /* border: 1px solid v-bind("groupStore.getWgColor()"); */
+  /* padding: 1px; */
 }
 
 .groups {
+  padding: 4px;
   overflow-y: auto;
   max-height: 390px;
 }
 
+::-webkit-scrollbar-thumb {
+  background: v-bind("groupStore.getWgColor()");
+}
+
 @media only screen and (max-width: 700px) {
-  .wg-header {
-    display: block;
-  }
-
-  .new-group {
-    width: 100%;
-  }
-
   .groups {
     max-height: 100%;
   }
