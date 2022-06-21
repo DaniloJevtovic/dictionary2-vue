f03e4ef5f657b969a2160ebfe95b125f6802e96f
Lemur <danilo_jevtovic@hotmail.com>
2022-06-21 21:10:20 +0200

    promjene u navbaru i prikazu

--- src/views/dictionary/DictionaryHome.vue
+++ src/views/dictionary/DictionaryHome.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="home">
-    <div class="navbar">
-      <DictionaryNavbar />
-    </div>
+    <DictionaryNavbar />
 
     <div class="container">
       <router-view :key="$route.path" />
@@ -22,17 +20,7 @@ import DictionaryNavbar from "./DictionaryNavbar.vue";
   padding: 5px;
 }
 
-.navbar {
-  display: none; /**samo kad je sirina manja od 700px */
-}
-
 .container {
   height: 100%;
-}
-
-@media only screen and (max-width: 700px) {
-  .navbar {
-    display: block;
-  }
 }
 </style>
--- src/views/dictionary/DictionaryNavbar.vue
+++ src/views/dictionary/DictionaryNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navbar">
     <router-link :to="{ name: 'User' }" active-class="active"
-      >Hi, Lemur</router-link
+      >Hi, {{ userStore.user.firstName }}</router-link
     >
     |
     <!-- <button @click="showList = !showList">Rjecnici</button> -->
@@ -10,7 +10,11 @@
       Rjecnici
     </router-link>
     |
-    <router-link :to="{ name: 'Login' }">Logout</router-link>
+    <router-link
+      :to="{ name: 'Login' }"
+      @click.prevent="logout, userStore.logout()"
+      >Logout</router-link
+    >
   </div>
 
   <div v-if="showList" class="menu">
@@ -22,7 +26,12 @@
 import { ref } from "vue";
 import DictionariesMenu from "./DictionariesMenu.vue";
 import { useDictionaryStore } from "../../stores/dictionaries.js";
+import { useUserStore } from "../../stores/users.js";
+import useAUTH from "../../composables/useAUTH.js";
+
+const { logout } = useAUTH();
 const dictionaryStore = useDictionaryStore();
+const userStore = useUserStore();
 
 const showList = ref(false);
 </script>
@@ -32,6 +41,7 @@ const showList = ref(false);
   text-align: center;
   background: darkblue;
   color: cyan;
+  /* display: none; */
 }
 
 .menu {
@@ -46,5 +56,11 @@ a {
 
 a.active {
   color: hotpink;
+}
+
+@media only screen and (max-width: 700px) {
+  .navbar {
+    display: block;
+  }
 }
 </style>
