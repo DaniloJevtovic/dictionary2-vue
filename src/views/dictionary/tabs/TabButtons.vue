5887c676617adfaa84dc220f90f24bd293e8a87b
Lemur <danilo_jevtovic@hotmail.com>
2022-06-22 14:02:33 +0200

    tabovi kao dugmici i selekcija

---
+++ src/views/dictionary/tabs/TabButtons.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- tabovi u header-u rjecnika -->
+  <div class="tab-btns">
+    <button
+      v-for="(_, tab) in tabs"
+      :key="tab"
+      :class="['tab-button', { active: tabStore.currentTab === tab }]"
+      @click="tabStore.currentTab = tab"
+    >
+      {{ tab }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { useTabStore } from "../../../stores/tabs.js";
+import { useDictionaryStore } from "../../../stores/dictionaries.js";
+
+const props = defineProps({
+  tabs: [Object],
+});
+
+const tabStore = useTabStore();
+const dictionaryStore = useDictionaryStore();
+</script>
+
+<style scoped>
+.tab-button.active {
+  background: v-bind("dictionaryStore.dictionary.color");
+  color: black;
+}
+
+@media only screen and (max-width: 700px) {
+  .tab-btns {
+    display: none;
+  }
+}
+</style>
---
+++ src/views/dictionary/tabs/TabSelect.vue
@@ -0,0 +1,33 @@
+<template>
+  <select class="tab-select" v-show="true" v-model="tabStore.currentTab">
+    <option :value="tab" v-for="(_, tab) in tabs" :key="tab">
+      {{ tab }}
+    </option>
+  </select>
+</template>
+
+<script setup>
+import { useTabStore } from "../../../stores/tabs.js";
+import { useDictionaryStore } from "../../../stores/dictionaries.js";
+
+const props = defineProps({
+  tabs: [Object],
+});
+
+const tabStore = useTabStore();
+const dictionaryStore = useDictionaryStore();
+</script>
+
+<style scoped>
+.tab-select {
+  color: black;
+  background: v-bind("dictionaryStore.dictionary.color");
+  display: none;
+}
+
+@media only screen and (max-width: 700px) {
+  .tab-select {
+    display: block;
+  }
+}
+</style>
