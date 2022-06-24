8066bf283853da6cfb09caca2418b331d22e989c
Lemur <danilo_jevtovic@hotmail.com>
2022-06-24 13:51:01 +0200

    prijava i registracija

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,16 +1,69 @@
 <template>
-  <form>
-    <h4 style="margin: 0px; background: cyan; color: darkblue">Login</h4>
+  <form @submit.prevent="login">
+    <h4>Login</h4>
 
-    <input type="email" placeholder="email" required />
-    <input type="password" placeholder="password" required />
+    <input type="email" v-model="loginReq.email" placeholder="email" required />
+    <input
+      type="password"
+      v-model="loginReq.password"
+      placeholder="password"
+      required
+    />
+
+    <div v-if="error" class="error">{{ error }}</div>
 
     <button>Login</button>
   </form>
 </template>
 
 <script setup>
+import { reactive, ref, watch } from "vue";
+import useAUTH from "../../composables/useAUTH.js";
+import { useUserStore } from "../../stores/users.js";
+import { useRouter } from "vue-router";
+import { useToastStore } from "../../stores/toast.js";
+
+const loginReq = reactive({ email: "", password: "" });
+const { auth } = useAUTH();
+
+const userStore = useUserStore();
+const router = useRouter();
+const toastStore = useToastStore();
+
+const error = ref("");
+
+async function login() {
+  console.log(loginReq);
+  let res = await auth("login", loginReq);
+
+  if (res.status !== 200) {
+    console.log(res.response);
+    error.value = res.response.data;
+  } else {
+    error.value = "";
+    console.log(res);
+    localStorage.setItem("token", res.data.accessToken);
+
+    userStore.getUserByEmail();
+    toastStore.showToast("Successfully logged in", "info");
+
+    router.push({ name: "DictionaryHome" });
+  }
+}
+
+watch(loginReq, () => {
+  error.value = "";
+});
 </script>
 
 <style scoped>
+h4 {
+  margin: 0px;
+  background: cyan;
+  color: darkblue;
+}
+
+.error {
+  background: red;
+}
 </style>
--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,26 +1,95 @@
 <template>
-  <form>
-    <h4 style="margin: 0px; background: cyan; color: darkblue">
-      Register
-    </h4>
+  <form @submit.prevent="register">
+    <h4>Register</h4>
 
-    <input type="text" placeholder="full name" required />
-    <input type="text" placeholder="your language" />
-    <input type="email" placeholder="email" required />
-    <input type="password" placeholder="password" required />
-    <input type="password" placeholder="repeat password" required />
-    <input type="text" placeholder="your language" />
+    <input
+      type="text"
+      v-model="regReq.firstName"
+      placeholder="firstName"
+      required
+    />
+
+    <input
+      type="text"
+      v-model="regReq.lastName"
+      placeholder="lastName"
+      required
+    />
+
+    <input type="email" v-model="regReq.email" placeholder="email" required />
+
+    <input
+      type="password"
+      v-model="regReq.password"
+      placeholder="password"
+      required
+    />
+    <!-- <input type="password" placeholder="repeat password" required /> -->
+    <input type="text" v-model="regReq.language" placeholder="your language" />
+
+    <div v-if="error" class="error">{{ error }}</div>
 
     <button>Register</button>
   </form>
 </template>
 
 <script setup>
+import { reactive, ref } from "@vue/reactivity";
+import { useRouter } from "vue-router";
+import { useToastStore } from "../../stores/toast.js";
+import useAUTH from "../../composables/useAUTH.js";
+import { watch } from "@vue/runtime-core";
+
+const { auth } = useAUTH();
+
+const regReq = reactive({
+  firstName: "",
+  lastName: "",
+  language: "",
+  email: "",
+  password: "",
+});
+
+const router = useRouter();
+const error = ref("");
+const toastStore = useToastStore();
+
+async function register() {
+  let res = await auth("register", regReq);
+
+  if (res.status !== 201) {
+    console.log(res.response);
+    error.value = res.response.data;
+  } else {
+    error.value = "";
+    console.log(res);
+
+    toastStore.showToast("Successfully registered", "info");
+    router.push({ name: "Login" });
+  }
+}
+
+watch(
+  () => regReq.email,
+  () => {
+    error.value = "";
+  }
+);
 </script>
 
 <style scoped>
 .register-form {
   width: 50%;
   margin: auto;
+}
+
+h4 {
+  margin: 0px;
+  background: cyan;
+  color: darkblue;
+}
+
+.error {
+  background: red;
 }
 </style>
