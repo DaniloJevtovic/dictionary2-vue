1e6ea810f105a25c2c83e095b22e4fbe1d709070
Lemur <danilo_jevtovic@hotmail.com>
2022-06-24 13:51:50 +0200

    pocetna stranica za ulogovanog korisnika

---
+++ src/views/auth/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h4>Welcome to dictionary app</h4>
+
+    <div>
+      <router-link :to="{ name: 'AllDictionaries' }" active-class="active">
+        My dictionaries
+      </router-link>
+    </div>
+
+    <div>
+      <router-link :to="{ name: 'User' }">View your profile</router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+a {
+  color: darkblue;
+}
+</style>
