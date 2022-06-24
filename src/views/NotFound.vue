91c39a51f00ea23c9598be4b0bf6b999cc64ab38
Lemur <danilo_jevtovic@hotmail.com>
2022-06-24 13:52:22 +0200

    not found stranica

---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button @click="$router.go(-1)">back</button>
+
+    <h3>Page Not Found :(</h3>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
