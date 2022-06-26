c56211cd23012852cef16eb2c4a81e2eebafeb3c
Lemur <danilo_jevtovic@hotmail.com>
2022-06-26 13:06:31 +0200

    informacije o korisniku

--- src/views/auth/User.vue
+++ src/views/auth/User.vue
@@ -1,35 +1,80 @@
 <template>
   <div :class="settingsStore.dark === true ? 'dark-theme' : 'white-theme'">
-    <button @click="$router.go(-1)" class="back-btn">&#8592; Go Back</button>
+    <!-- <button @click="$router.go(-1)" class="back-btn">&#8592; Go Back</button> -->
 
     <form>
       <h4 style="margin: 0px; background: cyan; color: darkslateblue">
         User settings
       </h4>
 
-      <input type="text" placeholder="full name" required />
-      <input type="text" placeholder="your language" />
-      <input type="email" placeholder="email" required />
+      <input
+        type="text"
+        v-model="updateUser.firstName"
+        placeholder="first name"
+        required
+      />
+
+      <input
+        type="text"
+        v-model="updateUser.lastName"
+        placeholder="first name"
+        required
+      />
+
+      <input
+        type="email"
+        v-model="updateUser.email"
+        placeholder="email"
+        disabled
+        required
+      />
       <input type="password" placeholder="password" required />
       <input type="password" placeholder="repeat password" required />
-      <input type="text" placeholder="your language" />
 
-      <button @click="saveChanges">Save changes</button>
+      <input
+        type="text"
+        v-model="updateUser.language"
+        placeholder="your language"
+      />
+
+      <button @click.prevent="saveChanges" :disabled="disabledBtns">
+        Save changes
+      </button>
+      <!-- &nbsp;
+      <button @click.prevent="cancelChanges" >Cancel</button> -->
     </form>
 
     <AppSettings />
 
-    <br>
+    <br />
   </div>
 </template>
 
 <script setup>
+import { reactive, watch, ref, computed } from "@vue/runtime-core";
 import { useSettingsStore } from "../../stores/settings.js";
+import { useUserStore } from "../../stores/users.js";
 import AppSettings from "./AppSettings.vue";
 
 const settingsStore = useSettingsStore();
+const userStore = useUserStore();
 
-async function saveChanges() {}
+const updateUser = ref({ ...userStore.user });
+const disabledBtns = ref(true);
+
+watch(updateUser.value, () => {
+  disabledBtns.value = false;
+});
+
+async function saveChanges() {
+  console.log(updateUser.value);
+  disabledBtns.value = true;
+}
+
+// function cancelChanges() {
+//   disabledBtns.value = true;
+//   updateUser.value = { ...userStore.user };
+// }
 </script>
 
 <style scoped>
