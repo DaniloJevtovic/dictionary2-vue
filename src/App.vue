3fb6a5e18d264f7c1174b3bbafa8d3effd431a19
Lemur <danilo_jevtovic@hotmail.com>
2022-07-03 20:42:48 +0200

    tranzicije

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <template>
   <router-view></router-view>
+
+  <!-- <router-view v-slot="{ Component, route }">
+    <transition name="fade">
+      <component :is="Component" :key="route.path" />
+    </transition>
+  </router-view> -->
 </template>
 
 <script setup>
+import { onMounted } from "@vue/runtime-core";
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+
+// onMounted(() => {
+//   window.onpopstate = (event) => {
+//     if (
+//       window.localStorage.getItem("token") !== null &&
+//       route.path == "/login"
+//     ) {
+//       router.push("/welcome");
+//     }
+//   };
+// });
 </script>
 
 <style>
