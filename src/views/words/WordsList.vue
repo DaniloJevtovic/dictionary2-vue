2f4bcad9cc9f276cafa6ce614d18287fb37bdaab
Lemur <danilo_jevtovic@hotmail.com>
2022-07-27 05:39:06 +0200

    lista rjeci kao posebna komponenta

---
+++ src/views/words/WordsList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div v-if="wordStore.words.length">
+      <div v-for="(word, index) in wordStore.words" :key="word.id">
+        <Word :word="word" :idx="index" />
+      </div>
+    </div>
+    <div v-else>
+      <Spiner />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Word from "./Word.vue";
+import Spiner from "../../components/Spinner.vue";
+
+import { useWordStore } from "../../stores/words.js";
+import { useDictionaryStore } from "../../stores/dictionaries.js";
+import { onMounted } from "@vue/runtime-core";
+
+const wordStore = useWordStore();
+const dictionaryStore = useDictionaryStore();
+
+onMounted(() => {
+  wordStore.filter = "sort=id,desc";
+  wordStore.search = "";
+  wordStore.getWords("DIC", dictionaryStore.dictionary.id);
+});
+</script>
+
+<style scoped>
+</style>
